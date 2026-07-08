47c4a7ac3ae0bcb8fa43adc4e4447002570f014a
Mason Carver <carver.mason@icloud.com>
2026-07-08 12:48:02 -0700

    added/fixed tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,9 @@
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { test, expect } from "vitest";
 import App from "./App";
 
 test("renders navigation links", () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  render(<App />);
 
   expect(screen.getByText("Home")).toBeInTheDocument();
   expect(screen.getByText("Spacecrafts")).toBeInTheDocument();
